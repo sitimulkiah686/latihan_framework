44a8a678029c3e16d9fd8dae48d520cc06961ac4
sitimulkiah686 <sitimulkiah686@gmail.com>
2020-02-29 13:54:22 +0700

    update tgl 29 Feb 2020

---
+++ latihan_javascript/js/array.js
@@ -0,0 +1,14 @@
+var buah = ["apel", "jeruk", "stroberi"];
+var mobil = ["Toyota", "Ferrari", "Lexus"];
+var pegawai = ["Billy", "Bob", "John"];
+var bilanganPrima = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+var acak = [2, "Supir", undefined, null, true, 2.51];
+arrayContoh = ["nol", "satu", "dua"];
+
+window.alert(arrayContoh[0]);
+// 0 merupakan element pertama array
+// dalam hal ini nilainya "nol"
+
+arrayContoh = ["Ahmad", "Budi"];
+nomor = 1;
+window.alert(arrayContoh[nomor]); // Ahmad
---
+++ latihan_javascript/js/confirm.js
@@ -0,0 +1,4 @@
+var konfirmasi = window.confirm("Apakah Anda Sudah Yakin ?");
+
+if (konfirmasi) console.log("Anda Yakin Sekali.");
+else console.log("Anda Berarti tidak Yakin .");
